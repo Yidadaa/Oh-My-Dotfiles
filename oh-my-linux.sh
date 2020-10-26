a215b5e1041ea16ed25a604abf703ffd94f1459b
Yifei Zhang <yidadaa@qq.com>
2020-10-26 18:20:12 +0800

    Update oh-my-linux.sh

--- oh-my-linux.sh
+++ oh-my-linux.sh
@@ -13,8 +13,9 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 # 侧边栏
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
 vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
-# git 显示
-git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git ~/.vim/bundle/nerdtree-git-plugin
+# git 显示，由于兼容性问题，暂时关闭该配置项
+# git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git ~/.vim/bundle/nerdtree-git-plugin
+
 # 同步 VIM 配置文件
 curl https://raw.githubusercontent.com/Yidadaa/Linux-Desktop-Config/master/.vimrc > ~/.vimrc # VIM配置
 
