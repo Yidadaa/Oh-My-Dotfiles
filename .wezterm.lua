32b8c64a3df9ad4756eadb83692e5bdf802e29b5
Yifei Zhang <yidadaa@qq.com>
2022-10-26 03:27:56 +0000

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,4 +4,8 @@ return {
   color_scheme = "Catppuccin Mocha", -- or Macchiato, Frappe, Latte
   font_size = 16.0,
   use_fancy_tab_bar = false,
+  font = wezterm.font_with_fallback {
+    'JetBrainsMono Nerd Font Mono',
+    'Noto Sans Mono CJK SC'
+  }
 }
