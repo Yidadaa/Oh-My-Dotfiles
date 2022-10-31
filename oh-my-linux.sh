4f36510cf8bf143ed64c6eca79ec295b4b5d2906
Yifei Zhang <yidadaa@qq.com>
2022-10-31 15:47:13 +0800

    Update oh-my-linux.sh

--- oh-my-linux.sh
+++ oh-my-linux.sh
@@ -2,9 +2,7 @@
 
 # 命令行配置
 echo 'Installing Common Tools...'
-sudo apt-get install -y curl
-sudo apt-get install -y git
-sudo apt-get install -y zsh
+sudo apt-get install -y curl git zsh tmux procs ripgrep fzf fd-find xclip delta
 
 echo 'Setting up VIM'
 # 安装 VIM 插件
@@ -20,12 +18,14 @@ curl https://raw.githubusercontent.com/Yidadaa/Linux-Desktop-Config/master/.vimr
 
 # Tmux
 echo 'Setting up Tmux...'
-sudo apt install -y tmux
 curl https://raw.githubusercontent.com/Yidadaa/Linux-Desktop-Config/master/.tmux.conf > ~/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # wezterm
 curl https://raw.githubusercontent.com/Yidadaa/Linux-Desktop-Config/master/.wezterm.lua > ~/.wezterm.lua
+
+# zsh
+curl https://raw.githubusercontent.com/Yidadaa/Linux-Desktop-Config/master/.zshrc > ~/.zshrc
 
 # 安装oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
@@ -33,8 +33,6 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 # 命令行工具
 echo 'Installing awesome cli tools...'
 pip3 install tldr # 用于替代 man 命令
-sudo apt install -y procs # 用于替代 ps 命令
-sudo apt install -y ripgrep fzf fd-find # 搜索工具
 # 别名：alias fd=fdfind
 
 sudo apt install -o Dpkg::Options::="--force-overwrite" bat ripgrep -y # 用于替代 cat 工具
@@ -59,7 +57,5 @@ fif() {
 	| awk -F  ":" '/1/ {start = $2<5 ? 0 : $2 - 5; end = $2 + 5; print $1 " " $2 " " start ":" end}' \
 	| fzf --preview 'bat --wrap character --color always {1} --highlight-line {2} --line-range {3}' --preview-window wrap
 }
-
-sudo apt install -y xclip delta # 剪切板工具，文件对比工具
 
 echo 'Done, enjoy coding :)'
